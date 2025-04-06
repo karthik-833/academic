b6c77f5b0b2900ac2f3228f90ae9fc9db8ad42c2
karthik-833 <pujarikarthik833@gmail.com>
2025-04-07 00:06:03 +0530

    Add files via upload

---
+++ projects/risc/Register_File.v
@@ -0,0 +1,26 @@
+
+module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
+
+    input clk,rst,WE3;
+    input [4:0]A1,A2,A3;
+    input [31:0]WD3;
+    output [31:0]RD1,RD2;
+    integer i;
+    reg [31:0] Register [31:0];
+
+    always @ (posedge clk)
+    begin
+        if(WE3 & (A3 != 5'h00))
+            Register[A3] <= WD3;
+    end
+
+    assign RD1 = (rst==1'b0) ? 32'd0 : Register[A1];
+    assign RD2 = (rst==1'b0) ? 32'd0 : Register[A2];
+
+    initial begin
+    for (i=0;i<32;i=i + 1 ) begin
+        Register[i] = i;
+        end
+    end
+
+endmodule
